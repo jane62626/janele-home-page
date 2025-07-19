479db89af957058fa90597e1769ce0b624392794
jane62626 <137153371+jane62626@users.noreply.github.com>
2025-07-19 11:40:18 +0800

    更新

--- src/config.js
+++ src/config.js
@@ -20,7 +20,8 @@ const config = {
   blur: 5, // 毛玻璃模糊效果
 
   // 我的标签
-  tags : ['乐观开朗','温柔体贴','随和亲切','冷静沉着','才思敏捷','风趣幽默','刚正不阿','善解人意'],
+  //tags : ['乐观开朗','温柔体贴','随和亲切','冷静沉着','才思敏捷','风趣幽默','刚正不阿','善解人意'],
+  //"tags": ["疯批年下", "强制骨科", "双向救赎", "大型犬攻",  "绝世好受", "男孕生子", "强攻强受"],
 
   // 默认背景壁纸
   background: {
@@ -52,18 +53,18 @@ const config = {
   //社交按钮
   socialPlatformIcons: [
     {icon:"mdi-github",link:"https://www.github.com/jane62626"},
-    {icon:"mdi-email",link:"mailto:leleo886@foxmail.com"},
+    {icon:"mdi-email",link:"mailto:ldjanelee@outlook.com"},
     {icon:"mdi-qqchat",link:"https://im.qq.com/"},
-    {icon:"mdi-wechat",link:"https://wx.qq.com/"},
+    {icon:"mdi-wechat",link:"https://monojson.com/s/cJmu4"},
     {icon:"mdi-youtube",link:"https://www.youtube.com"},
-    {icon:"mdi-facebook",link:"https://www.facebook.com"}
+    {icon:"mdi-twitter",link:"https://x.com"}
   ],
 
   //打字机
   typeWriterStrings: [
-    "如果你看到了这行字，说明我已经成功吸引到了你的注意力。",
-    "顶峰的少年，给了你所有细节，你却说我不是迪迦，给不了你想要的光。",
-    "心简单，世界就简单，幸福才会生长；心自由，生活就自由，到哪都有快乐。",
+    "希望树活着，花也是，火也是。","走过安徒生走过的路，从此我就会写童话故事啦。",
+    "我不在乎外部给我的标签，因为他们只是想记住我而已。",
+    "终有一日，寒冬会融于暖阳之下，而我，会化作你永不落幕的春天。",
     "生命太短，没有时间留给遗憾，若不是终点，请微笑一直向前。"
   ],
 
@@ -77,17 +78,14 @@ const config = {
   //壁纸数据 -----可以将壁纸文件上传到uhsea.com获取网络直链。若想调用api，请前往脚本自行修改逻辑
   wallpaper:{
     pic:[
-      {"title":"海洋女孩","preview":"/img/wallpaper/static/海洋女孩/image-pre.webp","url":"/img/wallpaper/static/海洋女孩/image.png"},
-      {"title":"书房夜晚","preview":"/img/wallpaper/static/书房夜晚/image-pre.webp","url":"/img/wallpaper/static/书房夜晚/image.png"},
-      {"title":"安逸舒适","preview":"/img/wallpaper/static/安逸舒适/image-pre.webp","url":"/img/wallpaper/static/安逸舒适/image.png"},
-      {"title":"jswcMaMj","preview":"https://file.uhsea.com/2503/76a4f03e8654d6e66bbbc7e8149c2fe33M.png","url":"https://file.uhsea.com/2503/5b8a20a5f2b269659911c9485d0d0c3f0F.png"},
-      {"title":"pgtTqoqq","preview":"https://file.uhsea.com/2503/caeae6b4fc1aba41bff4ad4ec59d6d8aA5.png","url":"https://file.uhsea.com/2503/45aa0a5a31a0ae95f7cc660e3ab4148eUT.png"},
-      {"title":"cvKMKhue","preview":"https://file.uhsea.com/2503/7a693bf725b501e0907ce40f18d2edc49P.png","url":"https://file.uhsea.com/2503/55e7191130d160a8e4421497308ffb67DT.png"},
-      {"title":"XpxvQVoP","preview":"https://file.uhsea.com/2503/f4b6804c7189357d3252aa0f981b59c4EF.png","url":"https://file.uhsea.com/2503/66359907715d46e0a4523da0142a6f63EU.png"},
-      {"title":"fVEEjEOA","preview":"https://file.uhsea.com/2503/ed0a4c0a43118ac4e2f39bab75ec19f4PI.png","url":"https://file.uhsea.com/2503/4adbf54af317a075c6b98483a552641f61.png"},
-      {"title":"jgnIKMpd","preview":"https://file.uhsea.com/2503/5d4d35ed1c286afc930184231456edc9BR.png","url":"https://file.uhsea.com/2503/3bf8025d8ad7ccefcf7e72c9368a245cLL.png"},
-      {"title":"mgqyySeh","preview":"https://file.uhsea.com/2503/38994dc4ef8659067537a3ea11af0927KB.png","url":"https://file.uhsea.com/2503/6e3b14778e879234826b9d5c793474ffOS.png"},
-      {"title":"dSXZfZp","preview":"https://file.uhsea.com/2503/22c27ea8c1e44209a223a28026146115LS.png","url":"https://file.uhsea.com/2503/8d1203a01e7a5a639caf91ca6c9608acVL.png"},
+      {"title":"珍珠少年","preview":"https://file.uhsea.com/2507/a897ae412dae1b7ef5526b9651cbbf933Z.webp","url":"https://file.uhsea.com/2507/8cd4f96990fc153492870a49223ca219X3.png"},
+      {"title":"腾瑞雨","preview":"https://file.uhsea.com/2507/dcc9b874ae30dc3c1e2dbd020f4b4bc5HS.webp","url":"https://file.uhsea.com/2507/7a82ec7dc1881d6148510009b06a4373IJ.png"},
+      {"title":"鬼夜曲","preview":"https://file.uhsea.com/2507/4531c4c9fd323247340f5d8738e27d64C7.webp","url":"https://file.uhsea.com/2507/e1655e686fbf5004231f6b2e4b5796fcZS.png"},
+      {"title":"天官赐福","preview":"https://file.uhsea.com/2507/e5d3b30c2cd1d9f034dcbfabf7cc3ad97V.webp","url":"https://file.uhsea.com/2507/0554a4363df7878c2c2b4b9ea1ed94fe0M.png"},
+      //{"title":"","preview":"","url":""},
+      //{"title":"jgnIKMpd","preview":"","url":""},
+      //{"title":"mgqyySeh","preview":"","url":""},
+      //{"title":"dSXZfZp","preview":"h","url":""},
       
     ],
     picMobile:[
@@ -95,43 +93,48 @@ const config = {
       {"title":"0002","preview":"/img/wallpaper/static-mobile/0002/image-pre.webp","url":"/img/wallpaper/static-mobile/0002/image.png"},
       {"title":"0003","preview":"/img/wallpaper/static-mobile/0003/image-pre.webp","url":"/img/wallpaper/static-mobile/0003/image.png"},
       {"title":"0004","preview":"/img/wallpaper/static-mobile/0004/image-pre.webp","url":"/img/wallpaper/static-mobile/0004/image.png"},
-      {"title":"DfNHPPcc","preview":"https://file.uhsea.com/2503/e3510f4d0286e37b828f74494230f0efM2.png","url":"https://file.uhsea.com/2503/97b50adb3d4cd6f1a4132f6d3e8c98483T.png"},
-      {"title":"WIOlrGSy","preview":"https://file.uhsea.com/2503/99162abfe07e18cfe6182ca3458561ef3A.png","url":"https://file.uhsea.com/2503/609532b992163117db9b341dde87b78f3C.png"},
-      {"title":"cZZwzhis","preview":"https://file.uhsea.com/2503/cf558fa1989c07726778c60be47b9928K8.png","url":"https://file.uhsea.com/2503/e27b91f1604f73d0444c807024ef7c9fI3.png"},
-      {"title":"aANKZHPX","preview":"https://file.uhsea.com/2503/0aa1cb875c8c7c20afd1aa72530294350K.png","url":"https://file.uhsea.com/2503/b09b65e20e176cb0d22dbc319981b5b45L.png"},
+      //{"title":"DfNHPPcc","preview":"https://file.uhsea.com/2503/e3510f4d0286e37b828f74494230f0efM2.png","url":"https://file.uhsea.com/2503/97b50adb3d4cd6f1a4132f6d3e8c98483T.png"},
+      //{"title":"WIOlrGSy","preview":"https://file.uhsea.com/2503/99162abfe07e18cfe6182ca3458561ef3A.png","url":"https://file.uhsea.com/2503/609532b992163117db9b341dde87b78f3C.png"},
+      //{"title":"cZZwzhis","preview":"https://file.uhsea.com/2503/cf558fa1989c07726778c60be47b9928K8.png","url":"https://file.uhsea.com/2503/e27b91f1604f73d0444c807024ef7c9fI3.png"},
+      //{"title":"aANKZHPX","preview":"https://file.uhsea.com/2503/0aa1cb875c8c7c20afd1aa72530294350K.png","url":"https://file.uhsea.com/2503/b09b65e20e176cb0d22dbc319981b5b45L.png"},
     ],
     video:[
       {
-        "title":"尼尔：机械纪元 团队",
-        "preview":"/img/wallpaper/dynamic/尼尔：机械纪元 团队/Nier-Automata-Team-pre.webm",
-        "url":"/img/wallpaper/dynamic/尼尔：机械纪元 团队/Nier-Automata-Team.webm"
+        "title":"鬼夜曲",
+        "preview":"https://file.uhsea.com/2507/60d78759699bd5845da089a4de698869TK.webm",
+        "url":"https://file.uhsea.com/2507/9c1e96aa5dfdfd051f959b418b19c6f3RV.webm"
       },
       {
-        "title":"向往航天的女孩",
-        "preview":"/img/wallpaper/dynamic/向往航天的女孩/Toy-Aeroplane-pre.webm",
-        "url":"/img/wallpaper/dynamic/向往航天的女孩/Toy-Aeroplane.webm"
+        "title":"王亿",
+        "preview":"https://file.uhsea.com/2507/f78e6dd807c5baeddc8e2bcf1cc59e74C4.webm",
+        "url":"https://file.uhsea.com/2507/3403382387954d976112ed079a0555c5XF.webm"
       },
-      {
-        "title":"世界很温柔《龙族》上杉绘梨衣",
-        "preview":"https://file.uhsea.com/2503/c048d174eee7df15de79a6cdc5c47697LT.webm",
-        "url":"https://file.uhsea.com/2503/320a926c4a3d6c7e0457823f7fd45e5dUC.webm"
-      },
+      //{
+        //"title":"世界很温柔《龙族》上杉绘梨衣",
+        //"preview":"https://file.uhsea.com/2503/c048d174eee7df15de79a6cdc5c47697LT.webm",
+        //"url":"https://file.uhsea.com/2503/320a926c4a3d6c7e0457823f7fd45e5dUC.webm"
+      //},
     ],
     videoMobile:[
       {
-        "title":"幻觉镇-gaako_illust",
-        "preview":"/img/wallpaper/dynamic-mobile/幻觉镇-gaako_illust/Hallucination_town-pre.mp4",
-        "url":"/img/wallpaper/dynamic-mobile/幻觉镇-gaako_illust/Hallucination_town.mp4"
+        "title":"言逸",
+        "preview":"https://file.uhsea.com/2507/776bca40ebbcb7051904c0db9724e436S9.mp4",
+        "url":"https://file.uhsea.com/2507/9629503e3cb52e6d00d16e6f71ea79460K.mp4"
       },
       {
-        "title":"chuva",
-        "preview":"/img/wallpaper/dynamic-mobile/chuva/chuva-pre.mp4",
-        "url":"/img/wallpaper/dynamic-mobile/chuva/chuva.mp4"
+        "title":"王亿",
+        "preview":"https://file.uhsea.com/2507/35588a98c06091edd49c021ad9bebbceOK.mp4",
+        "url":"https://file.uhsea.com/2507/e3c537915cd1b75c9dc9de49fbfeb217OT.mp4"
       },
       {
-        "title":"Doodle-小猫女仆降临",
-        "preview":"/img/wallpaper/dynamic-mobile/Doodle-小猫女仆降临/d12-pre.mp4",
-        "url":"/img/wallpaper/dynamic-mobile/Doodle-小猫女仆降临/d12.mp4"
+        "title":"珍珠花",
+        "preview":"https://file.uhsea.com/2507/4b82698bc77d747ee20cf0e08acb737fHX.mp4",
+        "url":"https://file.uhsea.com/2507/5e7264a8d81319f2fab413e75fe4b7b36F.mp4"
+      },
+      {
+        "title":"无根树",
+        "preview":"https://file.uhsea.com/2507/6f45c3b71a904d13ce5f74d2ba1a2943QS.mp4",
+        "url":"https://file.uhsea.com/2507/f57a37f53fb7a050296718d8da8f91fd7C.mp4"
       },
     ],
   },
